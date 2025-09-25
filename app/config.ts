f0824a5aa9c379b129dc4d10ff8595053c7a0866
deepesh agarwal <deepeshagarwal150106@gmail.com>
2025-09-25 18:43:54 +0530

    changed urls

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://localhost:3000"
-export const HOOKS_URL = "http://localhost:3002"
+export const BACKEND_URL = "https://integrator-backend-rfyd.onrender.com"
+export const HOOKS_URL = "https://integrator-hooks.onrender.com"
